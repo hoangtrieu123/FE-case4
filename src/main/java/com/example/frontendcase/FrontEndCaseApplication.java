b0eabb22cb62de818a88959c9c3a30b48fd72776
hoangtrieu123 <bimtrieu1998@gmail.com>
2022-09-06 09:03:16 +0700

    case

---
+++ src/main/java/com/example/frontendcase/FrontEndCaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.frontendcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontEndCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontEndCaseApplication.class, args);
+    }
+
+}
